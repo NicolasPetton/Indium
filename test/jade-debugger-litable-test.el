1192d19924a03d5ce8e7524d4f8dc0a1447f92dc
Nicolas Petton <nicolas@petton.fr>
2017-04-10 14:06:04 +0200

    Add unit tests for jade-debugger-litable

---
+++ test/jade-debugger-litable-test.el
@@ -0,0 +1,43 @@
+;;; jade-debugger-litable-test.el --- Test for jade-debugger-litable.el  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016-2017  Nicolas Petton
+
+;; Author: Nicolas Petton <nicolas@petton.fr>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+(require 'el-mock)
+
+(require 'jade-debugger-litable)
+
+(ert-deftest jade-debugger-litable-truncate-string-test ()
+  (let ((buffer-contents "let foo = 1;")
+        (short-string "short")
+        (long-string "very very very long string that overflows"))
+    (with-mock
+      (mock (window-width) => 50)
+      (with-temp-buffer
+        (insert buffer-contents)
+        (should (equal (jade-debugger-litable--overlay-string short-string)
+                       short-string))
+        (should (equal (jade-debugger-litable--overlay-string long-string)
+                       "very very very long string that ov..."))))))
+
+(provide 'jade-debugger-litable-test)
+;;; jade-debugger-litable-test.el ends here
