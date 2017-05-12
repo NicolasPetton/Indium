e2835f65adfd35ab143c2eb01234614b4067b14d
TatriX <tatrics@gmail.com>
2017-05-12 16:04:26 +0300

    Update network docs

--- indium-webkit.el
+++ indium-webkit.el
@@ -41,7 +41,7 @@
 (require 'indium-workspace)
 
 (defvar indium-webkit-cache-disabled nil
-  "Network cache disabled state.  If t disable cache when Indium starts.")
+  "Network cache disabled state.  If non-nil disable cache when Indium starts.")
 
 (defvar indium-webkit-completion-function "function getCompletions(type)\n{var object;if(type==='string')\nobject=new String('');else if(type==='number')\nobject=new Number(0);else if(type==='boolean')\nobject=new Boolean(false);else\nobject=this;var resultSet={};for(var o=object;o;o=o.__proto__){try{if(type==='array'&&o===object&&ArrayBuffer.isView(o)&&o.length>9999)\ncontinue;var names=Object.getOwnPropertyNames(o);for(var i=0;i<names.length;++i)\nresultSet[names[i]]=true;}catch(e){}}\nreturn resultSet;}")
 
