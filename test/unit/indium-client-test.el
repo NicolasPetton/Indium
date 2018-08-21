eee4db88ab190cfbf4ccfb8b0c2691eee9033944
Nicolas Petton <nicolas@petton.fr>
2018-08-21 14:26:21 +0200

    Add regression tests for GH issue #166
    * test/unit/indium-client-test.el: Add server message reading tests.

--- test/unit/indium-client-test.el
+++ test/unit/indium-client-test.el
@@ -30,5 +30,40 @@
       (expect (indium-client-start (lambda ())) :to-throw
 	      'user-error '("Cannot find the indium executable.  Please run \"npm install -g indium\"")))))
 
+(describe "Reading server messages"
+  (it "should not change the buffer reading an incomplete message"
+    (with-temp-buffer
+      (insert "{foo")
+      (indium-client--handle-data (current-buffer))
+      (expect (buffer-string) :to-equal "{foo")))
+
+  (it "should call `indium-client--handle-message' when reading a complete message"
+    (spy-on #'indium-client--handle-message)
+    (with-temp-buffer
+      (insert "{\"foo\": 1}\n")
+      (indium-client--handle-data (current-buffer))
+      (expect #'indium-client--handle-message :to-have-been-called-with '((foo . 1)))))
+
+  (it "should remove the linefeed char when reading a complete message"
+    (spy-on #'indium-client--handle-message)
+    (with-temp-buffer
+      (insert "{\"foo\": 1}\n")
+      (indium-client--handle-data (current-buffer))
+      (expect (buffer-string) :to-equal "")))
+
+  (it "should remove all linefeed chars when reading multiple complete messages"
+    (spy-on #'indium-client--handle-message)
+    (with-temp-buffer
+      (insert "{\"foo\": 1}\n{\"bar\": 2}\n")
+      (indium-client--handle-data (current-buffer))
+      (expect (buffer-string) :to-equal "")))
+
+  (it "should remove all linefeed chars but keep incomplete messages"
+    (spy-on #'indium-client--handle-message)
+    (with-temp-buffer
+      (insert "{\"foo\": 1}\n{\"bar\": 2}\n{\"baz")
+      (indium-client--handle-data (current-buffer))
+      (expect (buffer-string) :to-equal "{\"baz"))))
+
 (provide 'indium-client-test)
 ;;; indium-client-test.el ends here
