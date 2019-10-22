9614d63fa5a5126bd5b68d62410894371da081bf
Nicolas Petton <nicolas@petton.fr>
2019-10-22 23:28:08 +0200

    Bump Indium to v2.1.4
    * indium.el:
    * sphinx-doc/conf.py: Bump Indium version

--- indium.el
+++ indium.el
@@ -5,7 +5,7 @@
 ;; Author: Nicolas Petton <nicolas@petton.fr>
 ;; URL: https://github.com/NicolasPetton/indium
 ;; Keywords: tools, javascript
-;; Version: 2.1.3
+;; Version: 2.1.4
 ;; Package-Requires: ((emacs "25") (seq "2.16") (js2-mode "20140114") (js2-refactor "0.9.0") (company "0.9.0") (json-process-client "0.2.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
